0261c537da7c1e7cf0d624641e7e4ac120744224
MRNode Local <local@mrnode.invalid>
2026-01-29 12:52:35 +0200

    fix: remove invalid next config option

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    allowedDevOrigins: ["milkily-unluminiferous-joziah.ngrok-free.dev"],
-  },
 };
 
 export default nextConfig;
