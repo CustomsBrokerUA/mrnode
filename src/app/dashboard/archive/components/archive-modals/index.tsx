2fb5d68cd8e0ceac84ac52c6d5567b76e20200eb
MRNode Local <local@mrnode.invalid>
2026-02-01 16:38:54 +0200

    Remove ExportModal from archive modals exports

--- src/app/dashboard/archive/components/archive-modals/index.tsx
+++ src/app/dashboard/archive/components/archive-modals/index.tsx
@@ -2,7 +2,6 @@
 
 export { QuickPreviewModal } from './quick-preview-modal';
 export { default as DeletePeriodModal } from './delete-period-modal';
-export { default as ExportModal } from './export-modal';
 export { default as StatisticsSettingsModal } from './statistics-settings-modal';
 export { QuickPreview60 } from './quick-preview-60';
 export { QuickPreview61 } from './quick-preview-61';
